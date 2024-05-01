73f28089816cbb8587b232e0bc94dd529737ac3a
Kaio_Araujo <araujokaio93@gmail.com>
2024-05-01 18:52:35 -0300

    Criação do Header

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,22 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom"
-import MainPage from "./pages/MainPage"
-import Login from "./pages/Login"
-import Logout from "./pages/Logout"
-import About from "./pages/About"
-import Profile from "./pages/Profile"
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import MainPage from "./pages/MainPage";
+import Login from "./pages/Login";
+import Logout from "./pages/Logout";
+import About from "./pages/About";
+import Profile from "./pages/Profile";
+import Header from "./components/Header";
 
 export default function App() {
-  
-  return <BrowserRouter>
-  <Routes>
-     <Route path="/" element={< MainPage/>} />
-     <Route path="/Login" element={< Login />} />
-     <Route path="/Logout" element={< Logout />} />
-     <Route path="/About" element={< About />} />
-     <Route path="/Profile" element={< Profile />} />
-  </Routes>
-  </BrowserRouter>
-
+  return (
+    <BrowserRouter>
+      <Header />
+      <Routes>
+        <Route path="/" element={<MainPage />} />
+        <Route path="/Login" element={<Login />} />
+        <Route path="/Logout" element={<Logout />} />
+        <Route path="/About" element={<About />} />
+        <Route path="/Profile" element={<Profile />} />
+      </Routes>
+    </BrowserRouter>  
+  );
 }
---
+++ src/components/Header.jsx
@@ -0,0 +1,41 @@
+import {FaSearch} from 'react-icons/fa';
+import { Link } from 'react-router-dom';
+
+export default function Header() {
+  return (
+    <header className='bg-red-700 shadow-md'>
+      <div className='flex justify-between items-center max-w-6x1 mx-auto p-3'>
+        <Link to="/">
+          <h1 className='font-bold text-sm sm:text-xl flex flex-wrap'>
+            <span className='text-slate-900'>Imobiliária Real - Corretagem e Vendas</span>
+          </h1>
+        </Link>
+        <form className='bg-slate-100 p-3 rounded-lg flex items-center'>
+          <input 
+            type="text" 
+            placeholder='Pesquisar:  ... ' 
+            className='bg-transparent focus:outline-none w-24 sm:w-64'
+          />
+          <FaSearch className='text-slate-600'/>
+        </form>
+        <ul className='flex gap-4'>
+          <Link to='/'>
+            <li className='hidden sm:inline text text-slate-800 hover:underline'>
+              Página Principal
+            </li>
+          </Link>
+          <Link to="About">
+            <li className='hidden sm:inline text text-slate-800 hover:underline'>
+              Sobre
+            </li>
+          </Link>
+          <Link to="Profile">
+            <li className='hidden sm:inline text text-slate-800 hover:underline'>
+              Login
+            </li>
+          </Link>
+        </ul>
+      </div>
+    </header>
+  );
+}
