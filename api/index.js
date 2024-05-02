c6d66c879de6be8336e44ef23b51ad98cc2246f9
Kaio_Araujo <araujokaio93@gmail.com>
2024-05-02 18:29:36 -0300

    Criando o servidor e conectando ao MongoDB

--- api/index.js
+++ api/index.js
@@ -1,6 +1,19 @@
 import express from 'express';
+import mongoose from 'mongoose';
+import dotenv from 'dotenv';
+dotenv.config();
+
+// Definindo a opção strictQuery para evitar o aviso de depreciação
+mongoose.set('strictQuery', false);
+
+mongoose.connect(process.env.MONGO).then(() => { 
+    console.log("Conectado ao MongoDB !")
+}).catch((err) => {
+    console.log(err);
+});
 
 const app = express();
 
-app.listen(3000, () => { console.log("O servidor está rodando na porta 3000")});
-
+app.listen(3000, () => { 
+    console.log("O servidor está rodando na porta 3000");
+});
